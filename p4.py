474e9d45013d58603f4e93e47d28d9a9d650602d
George <georgehender997@gmail.com>
2023-02-12 13:35:07 -0500

    py

---
+++ p4.py
@@ -0,0 +1,15 @@
+def max_num(a, b, c):
+    return max([a, b, c])
+
+print(max_num(100, 44, 14))
+
+def mult_list(a, b, c):
+    return(a * b * c)
+
+print(mult_list(4, 14, 3))
+
+def rev_string(str):
+    return str[::-1]
+
+print(rev_string("pink"))
+
