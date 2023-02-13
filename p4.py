497fe655574155463464c210cd3766cad80b5c0f
George <georgehender997@gmail.com>
2023-02-13 00:06:21 -0500

    it doesnt work

--- p4.py
+++ p4.py
@@ -13,3 +13,36 @@ def rev_string(str):
 
 print(rev_string("pink"))
 
+def num_within(a, b, c):
+    return a in range(b, c+1)
+
+print(num_within(14, 7, 8))
+print(num_within(4, 7, 14))
+
+
+triangle = [[1],[1,1]]
+def pascal(n):
+    if n < 1:
+        print("Can not create rows")
+    elif n == 1:
+        print(triangle[0])
+    else:
+        row_num = 2
+        while len(triangle) < n:
+            row = []
+            row_prev = triangle[row_num - 1]
+            length = len(row_prev)+1
+            for i in range(length):
+                if i == 0:
+                    row.append(1)
+                elif i > 0 and i < length-1:
+                    row.append(triangle[row_num-1][i-1]+triangle[row_num-1][i])
+                else:
+                    row.append(1)
+                triangle.append(row)
+                row_num += 1
+            for row in triangle:
+                print(row)
+
+pascal(2)
+pascal(5)
